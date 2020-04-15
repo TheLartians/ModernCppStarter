e542d9ac53e9b8aed72c6b6352a8e3c859ee595e
Lars Melchior <TheLartians@users.noreply.github.com>
2020-04-15 09:08:43 +0200

    turns out only clang understands strongly typed enums

--- source/greeter.cpp
+++ source/greeter.cpp
@@ -6,10 +6,7 @@ Greeter::Greeter(std::string _name) : name(_name) {}
 
 std::string Greeter::greet(LanguageCode lang) const {
   switch (lang) {
-#if defined(_WIN32) || defined(WIN32)
-    // this silences a MSVC warning as it does not seem to understand strongly-typed enums
     default:
-#endif
     case LanguageCode::EN:
       return "Hello, " + name + "!";
     case LanguageCode::DE:
