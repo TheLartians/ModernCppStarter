cde02f79cc7e4104d0a78f6d1876a0bc8d4dcc07
Lars Melchior <TheLartians@users.noreply.github.com>
2020-09-02 12:36:48 +0200

    add proper include guard to tools, fixes #57

--- cmake/tools.cmake
+++ cmake/tools.cmake
@@ -1,10 +1,7 @@
 # this file contains a list of tools that can be activated and downloaded on-demand each tool is
 # enabled during configuration by passing an additional `-DUSE_<TOOL>=<VALUE>` argument to CMake
 
-# only activate tools for top level project
-if(NOT PROJECT_SOURCE_DIR STREQUAL CMAKE_SOURCE_DIR)
-  return()
-endif()
+include_guard(DIRECTORY)
 
 include(${CMAKE_CURRENT_LIST_DIR}/CPM.cmake)
 
