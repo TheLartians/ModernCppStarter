f59c0cfbee7343a610fd15a24309da44965d75de
ClausKlein <claus.klein@arcormail.de>
2021-02-16 18:25:02 +0100

    one more try with dll on windows
    cleanup code too

--- include/greeter/greeter.h
+++ include/greeter/greeter.h
@@ -7,12 +7,12 @@
 namespace greeter {
 
   /**  Language codes to be used with the Greeter class */
-  enum class GREETER_EXPORT LanguageCode { EN, DE, ES, FR };
+  enum class LanguageCode { EN, DE, ES, FR };
 
   /**
    * @brief A class for saying hello in multiple languages
    */
-  class GREETER_EXPORT Greeter {
+  class Greeter {
     std::string name;
 
   public:
@@ -20,14 +20,15 @@ namespace greeter {
      * @brief Creates a new greeter
      * @param name the name to greet
      */
-    explicit Greeter(std::string _name);
+    explicit GREETER_EXPORT Greeter(std::string _name);
 
     /**
      * @brief Creates a localized string containing the greeting
      * @param lang the language to greet in
      * @return a string containing the greeting
      */
-    [[nodiscard]] auto greet(LanguageCode lang = LanguageCode::EN) const -> std::string;
+    [[nodiscard]] auto GREETER_EXPORT greet(LanguageCode lang = LanguageCode::EN) const
+        -> std::string;
   };
 
 }  // namespace greeter
